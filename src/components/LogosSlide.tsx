8a00b49b2778515beff250752dfcf23dccada9a0
bonox2 <illiagoncharov99@gmail.com>
2023-07-19 23:20:49 -0400

    logos slide animation error

---
+++ src/components/LogosSlide.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+
+export default function LogosSlide() {
+  return (
+    <div className="mt-14 px-12 overflow-hidden whitespace-nowrap ">
+      <div className="logos_animation">
+        <Image
+          src="/amazon_logo.svg"
+          alt="Amazon logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/dribbble_logo.svg"
+          alt="Dribbble logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px]  "
+        />
+        <Image
+          src="/hubspot_logo.svg"
+          alt="Hub Spot logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/notion_logo.svg"
+          alt="Notion logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/netflix_logo.svg"
+          alt="Netflix logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/zoom_logo.svg"
+          alt="Zoom logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+      </div>
+      <div className="logos_animation">
+        <Image
+          src="/amazon_logo.svg"
+          alt="Amazon logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/dribbble_logo.svg"
+          alt="Dribbble logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px]  "
+        />
+        <Image
+          src="/hubspot_logo.svg"
+          alt="Hub Spot logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/notion_logo.svg"
+          alt="Notion logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/netflix_logo.svg"
+          alt="Netflix logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+        <Image
+          src="/zoom_logo.svg"
+          alt="Zoom logo"
+          width={125}
+          height={50}
+          className="mx-12 w-[130px] h-[50px] "
+        />
+      </div>
+    </div>
+  );
+}
--- src/pageSections/Hero.tsx
+++ src/pageSections/Hero.tsx
@@ -1,35 +1,41 @@
-import Image from 'next/image'
+import Image from "next/image";
+import LogosSlide from "../components/LogosSlide";
 
 export default function Hero() {
-    return(
-        <section className='mt-16'>
-            <div className='container w-[1240px] m-auto flex justify-between items-start'>
-                <div className='flex flex-col justify-start  items-start gap-9'>
-                    <h1 className='text-6xl'>Navigating the digital landscape for success</h1>
+  return (
+    <section className="mt-16">
+      <div className="container w-[1240px] m-auto ">
+        <div className="flex justify-between items-start">
+          <div className="flex flex-col justify-start  items-start gap-9">
+            <h1 className="text-6xl">
+              Navigating the digital landscape for success
+            </h1>
 
-                    <p className='text-xl'>Our digital marketing agency helps businesses grow and succeed online through 
-                        a range of services including SEO, PPC, social media marketing, and content 
-                        creation.
-                    </p>
+            <p className="text-xl">
+              Our digital marketing agency helps businesses grow and succeed
+              online through a range of services including SEO, PPC, social
+              media marketing, and content creation.
+            </p>
 
-                    <button className="flex text-center justify-center items-center whitespace-nowrap text-white
-                     bg-[#191A23] rounded-2xl px-9 py-5 text-xl ">Book a Consultation</button>
-                </div>
-                <Image
-                    src="/hero-illustration.svg"
-                    alt="hero"
-                    width={600}
-                    height={515}
-                    className='object-contain w-[600px] h-[515px] ml-28'
-                />
-            </div>
-            <div>
-                <Image
-                    src="/amazon_logo.svg"
-                    alt="amazon"
-                    width={125}
-                    height={50}
-            </div>
-        </section>
-    )
+            <button
+              className="flex text-center justify-center items-center whitespace-nowrap text-white
+                     bg-[#191A23] rounded-2xl px-9 py-5 text-xl "
+            >
+              Book a Consultation
+            </button>
+          </div>
+          <Image
+            src="/hero-illustration.svg"
+            alt="hero"
+            width={600}
+            height={515}
+            className="object-contain w-[600px] h-[515px] ml-28"
+          />
+        </div>
+      <LogosSlide />
+        
+      </div>
+      
+    </section>
+  );
 }
